b2e2e17f3ccbbe00be5deb2cefb5956dcce81934
amerso2 <92588088+amerso2@users.noreply.github.com>
2021-10-15 17:32:05 +0300

    Add files via upload

---
+++ case_switch.js
@@ -0,0 +1,11 @@
+switch (prompt ("what is the weather")){
+case "snow":
+alert("wear jacket");
+break;
+case "rain":
+alert("take umblella");
+break;
+default:
+alert("take as you want");
+break;
+}
