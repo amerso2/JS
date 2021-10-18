974d9f175a33d6a9ff2c9469cd61c9d5ade8381b
amerso2 <92588088+amerso2@users.noreply.github.com>
2021-10-18 13:42:09 +0300

    Add files via upload

--- BuzzFeed.js
+++ BuzzFeed.js
@@ -1,17 +1,17 @@
 // Если делится только на на 3 то Buzz
-// Если делится только на на 5 то Feed
+// Если делится только на на 5 то Fizz
 // Если делится и на 3 и на 5 то BuzzFeed
 
 let num = 0;
 while (num<101){
 if (num %3 == 0 && num %5 !== 0){
-console.log("buzz");
+console.log("Buzz");
 }
 if(num %5 ==0 && num %3!==0){
-console.log("feed");
+console.log("Fizz");
 }
 if(num %5 ==0 && num %3==0){
-console.log("BuzzFeed");
+console.log("BuzzFizz");
 }
 else
 console.log(num);
